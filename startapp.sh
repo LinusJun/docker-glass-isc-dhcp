74715a82e5294f67ac08a672f85937a9f7bb7c70
djaydev <33335484+djaydev@users.noreply.github.com>
2019-06-21 09:03:31 -0400

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-touch /var/lib/dhcp/dhcpd.leases
+touch /var/lib/dhcp/dhcpd.leases &
 sed -i "/admin_password/s/glassadmin/$ADMINPASSWORD/g" /opt/glass-isc-dhcp/config/glass_config.json
 /usr/sbin/dhcpd -4 -f -d --no-pid -cf /etc/dhcp/dhcpd.conf > /var/log/dhcp.log 2>&1 &
 /usr/local/bin/npm start --prefix /opt/glass-isc-dhcp
-fg %1
